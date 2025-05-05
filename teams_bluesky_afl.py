f61466c8c89a18c096aae7b5371e72206ca8d9d7
Xueying Yuan <xueying.yuan@student.unimelb.edu.au>
2025-05-05 11:41:30 +1000

    Add a script to get bluesky's afl data

---
+++ teams_bluesky_afl.py
@@ -0,0 +1,7 @@
+AFL_TEAMS = [
+    '#AFL', '#AFLW', '#AFLDogsPower', '#AFLPiesCats', '#AFLSaintsFreo', '#AFLDonsNorth', ' #AFLLionsSuns', 
+    '#AFLHawksTigers', '#AFLEaglesDees', '#AFLSunsSwans', '#AFLBluesCats', '#AFLPowerNorth', '#AFLPiesDons',
+    'Adelaide Crows', 'Brisbane Lions', 'Carlton Blues', 'Collingwood Magpies', 'Essendon Bombers', 'Fremantle Dockers', 
+    'Geelong Cats', 'Gold Coast Suns', 'Greater Western Sydney Giants', 'Hawthorn Hawks', 'Melbourne Demons', 'North Melbourne Kangaroos', 
+    'Port Adelaide Power', 'Richmond Tigers', 'St Kilda Saints', 'Sydney Swans', 'West Coast Eagles', 'Western Bulldogs Bulldogs'
+]
